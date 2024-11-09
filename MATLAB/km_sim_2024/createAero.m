a5946d9ecb51a4cf643fb4836f3ed2c7d728a31d
danielruskin <d.p.ruskin@wustl.edu>
2024-11-09 11:31:49 -0600

    Update createAero.m

--- MATLAB/km_sim_2024/createAero.m
+++ MATLAB/km_sim_2024/createAero.m
@@ -12,7 +12,7 @@ end
 
 cd = importdata(aero_aoa_vs_cl_vs_cd);
 cdhold = cd.data;
-cdimport = cdhold(:,4); % Change columns based on XFLR csv columns (should
+cdimport = cdhold(:,12); % Change columns based on XFLR csv columns (should
                         % be something like RegularFlatWingsStatic)
 cdfinal = rmmissing(cdimport);
 
