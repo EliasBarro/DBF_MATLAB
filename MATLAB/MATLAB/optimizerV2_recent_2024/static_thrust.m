108f1bf6d235ee3d554344179f0ab54501bd3d46
danielruskin <d.p.ruskin@wustl.edu>
2024-11-09 11:41:45 -0600

    bad matlab folder

--- MATLAB/MATLAB/optimizerV2_recent_2024/static_thrust.m
+++
@@ -1,5 +0,0 @@
-function T_s = static_thrust(v_c, T_c, v_lof, T_lof)
-    fun = @(Ts,Tv,v) (Ts - 2*T_c)./(v_c.^2)*v.^2 + (3*T_c - 2*Ts)./v_c*v + Ts - Tv;
-    T_s = fzero(@(x) fun(x,T_lof,v_lof),40);
-    fcn = @(v) (T_s - 2*T_c)./(v_c.^2)*v.^2 + (3*T_c - 2*T_s)./v_c*v + T_s;
-end
